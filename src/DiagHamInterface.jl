01fcca87c54b9620945cb7c9351f12eb37e9f84d
AFeuerpfeil <andreas.feuerpfeil@gmail.com>
2025-12-11 17:03:30 -0500

    implement and expose function to install diagham automatically

--- src/DiagHamInterface.jl
+++ src/DiagHamInterface.jl
@@ -4,6 +4,7 @@ Interface to the DiagHam library.
 module DiagHamInterface
 
 export execute_diagham_script
+export install_diagham
 
 export read_matrix_from_txt
 export write_to_txt
@@ -17,6 +18,7 @@ using Preferences
 include("utility/backup.jl")
 include("utility/diagham_path.jl")
 include("utility/execute_script.jl")
+include("utility/diagham_install.jl")
 include("utility/fileending.jl")
 include("utility/numbers.jl")
 include("utility/standards.jl")
