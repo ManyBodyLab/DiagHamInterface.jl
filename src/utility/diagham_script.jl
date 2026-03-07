fdafe13a9b2d6d69744c81e661e5ecdc6e880476
AFeuerpfeil <andreas.feuerpfeil@gmail.com>
2026-03-07 17:45:20 -0500

    Add `diagham_command` to only create the command string. Also add option `wait` to `execute_diagham_script` to control whether the process should be waited for.

---
+++ src/utility/diagham_script.jl
@@ -0,0 +1,65 @@
+"""
+    diagham_command(execute; kwargs...)
+Construct a command string for running a DiagHam executable with specified kwargs as CLI flags.
+"""
+function diagham_command(execute::AbstractString; kwargs...)
+    return diagham_command([execute]; kwargs...)
+end
+function diagham_command(execute::AbstractVector{<:AbstractString};
+        diagham_path::AbstractString = get_diagham_path(),
+        kwargs...
+    )
+    warn_about_diagham_path()
+    ex_file = first(execute)
+    execute[1] = joinpath(diagham_path, ex_file)
+    execute = prod(execute)
+
+    ## Now, we add kwargs to the command:
+    for (k, v) in kwargs
+        execute *= diagham_kwarg(k, v)
+    end
+    return execute
+end
+
+function diagham_kwarg(key, value)
+    key = replace("$key", "_" => "-")
+    key_print = length(key) == 1 ? " -$(key)" : " --$(key)"
+    if value isa Bool
+        if value
+            return key_print
+        end
+    else
+        return "$(key_print) $(value)"
+    end
+    return ""
+end
+
+"""
+    execute_diagham_script(execute; kwargs...)
+
+Run a DiagHam executable. Kwargs are converted to CLI flags (`_` → `-`).
+Single-char keys use `-k`, multi-char use `--key`. Boolean `true` adds flag, `false` omits.
+
+The kwargs of `Cmd()` and `run()` are supported.
+"""
+function execute_diagham_script(execute::AbstractString; kwargs...)
+    return execute_diagham_script([execute]; kwargs...)
+end
+function execute_diagham_script(
+        execute::AbstractVector{<:AbstractString};
+        diagham_path::AbstractString = get_diagham_path(),
+        ignorestatus::Bool = false,
+        detach::Bool = false,
+        windows_verbatim::Bool = false,
+        windows_hide::Bool = false,
+        env = nothing,
+        dir = "",
+        wait::Bool = true,
+        cleanup::Bool = false,
+        kwargs...
+    )
+    execute = diagham_command(execute; diagham_path, kwargs...)
+    execute = Cmd(split(execute, " "))
+    cmd = Cmd(execute; ignorestatus = ignorestatus, detach = detach, windows_verbatim = windows_verbatim, windows_hide = windows_hide, env = env, dir = dir)
+    return run(cmd; wait = wait)
+end
